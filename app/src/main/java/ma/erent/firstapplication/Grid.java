fda7c6ecbb06341f576254f23dd499a4ebcdc749
majoch abdessamad <unleopard@gmail.com>
2020-04-28 00:41:31 +0100

    create project

---
+++ app/src/main/java/ma/erent/firstapplication/Grid.java
@@ -0,0 +1,14 @@
+package ma.erent.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Grid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+    }
+}
---
+++ app/src/main/java/ma/erent/firstapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package ma.erent.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_CREATE = "onCreate";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_CREATE, "mon application wase started");
+    }
+}
